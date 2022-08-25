40ff5a77a7f6ce10cf3da4b08cfab5d71c82a6f3
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-08-25 19:22:01 +0100

    working on stopwatch page

---
+++ stopwatch.js
@@ -0,0 +1,20 @@
+const stopwatchStart = ()=>{
+    Number(milliSec.innerHTML++)
+    let setTime = setTimeout('stopwatchStart()', '9');
+    if (Number(milliSec.innerHTML) == 100){
+        Number(milliSec.innerHTML = 0)
+        Number(seconds.innerHTML++)
+    }
+    else if (Number(seconds.innerHTML) == 60){
+        Number(seconds.innerHTML = 0)
+        Number(minutes.innerHTML++)
+    }
+    else if (Number(minutes.innerHTML) == 60){
+        Number(minutes.innerHTML = 0)
+        Number(hours.innerHTML++)
+    }
+
+}
+const stopwatchReload = ()=>{
+location.reload()
+}
