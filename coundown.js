893e92b166b26726d457b13b7bb9b156ea0363e2
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-08-26 15:59:43 +0100

    added countdown.js

---
+++ coundown.js
@@ -0,0 +1,36 @@
+const song = new Audio('Fireboy-DML-Sofri-(JustNaija.com).mp3');
+
+const countdownStart = ()=>{
+    let countdownSec = setSec.value
+    let countdownMin = setMin.value
+    let countdownHrs = setHrs.value
+    countdownSec--;
+    setSec.value = countdownSec
+    setTimeout(countdownStart, 1000);
+    if(countdownSec < 1 && countdownMin == 0){
+        song.play()
+        setSec.value = ""
+        setMin.value = ""
+        setHrs.value = ""
+    }
+    if(countdownSec < 0 && setMin.value > 0){
+        setSec.value = "59"
+    }
+    if(countdownSec < 0 && countdownMin > 0){
+        setMin.value--
+    }
+    if(countdownMin == 0 && setHrs.value > 0){
+        setMin.value = "60"
+    }
+    if(countdownMin == 0 && countdownHrs > 0){
+        setHrs.value--
+    }
+    // else if(((countdownHrs == "") || (countdownMin == "") || countdownSec == "")){
+    //     aa.innerHTML= "input digit"
+    // }
+}
+const countdownReload = ()=>{
+    location.reload()
+}
+
+
